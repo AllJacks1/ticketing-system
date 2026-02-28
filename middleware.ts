a4ff8d327f118ad54e0bd7664263a753408543ce
CodingMantis <k.tan.531880@umindanao.edu.ph>
2026-02-28 15:24:34 +0800

    feat(sys): protect pages from unauthenticated users

---
+++ middleware.ts
@@ -0,0 +1,38 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { createServerClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next();
+
+  const supabase = createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          response.cookies.set(name, value, options)
+        );
+      },
+    },
+  });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const isProtectedRoute = request.nextUrl.pathname.startsWith("/home");
+
+  if (isProtectedRoute && !user) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: ["/home/:path*"],
+};
